326db9030622c5d3966b31358a23e10741d7a2c7
Syngle <1271250334@qq.com>
2020-09-23 19:27:48 +0800

    封装axios模块

--- src/api/publicCode.js
+++ src/api/publicCode.js
@@ -1,6 +1,9 @@
 import svgCaptcha from 'svg-captcha'
 
 function getCode(ctx) {
+  console.log('xxxx')
+  const body = ctx.request.query
+  console.log(body)
   const captcha = svgCaptcha.create()
   ctx.body = {
     "code": 200,
